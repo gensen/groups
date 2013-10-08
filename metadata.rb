46ccb41413a56f88df57eff209d283952b26e670
Shrikant Patnaik <shrikant.patnaik@generalsensing.com>
2013-10-08 14:35:28 +0800

    moved groups cookbook to its own repository

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "General Sensing"
+maintainer_email "dev@generalsensing.com"
+license          "Apache 2.0"
+description      "Installs/Configures groups and adds the respective users"
+
+version          "0.1"
