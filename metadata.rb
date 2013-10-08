d6dc5d2baa88faf2cdc09d59955a05ed3546c865
Shrikant Patnaik <shrikant.patnaik@generalsensing.com>
2013-10-08 15:10:37 +0800

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "dev@generalsensing.com"
 license          "Apache 2.0"
 description      "Installs/Configures groups and adds the respective users"
 
-version          "0.1"
+version          "0.1.1"
